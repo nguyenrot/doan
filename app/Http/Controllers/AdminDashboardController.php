4afc3acc810164384c221c2962cdfe848ccfa908
nguyenrot <69588951+nguyenrot@users.noreply.github.com>
2021-09-16 11:52:07 +0700

    Install dashboard

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -2,15 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\chitietdonhang;
+use App\Models\danhgia;
+use App\Models\donhang;
+use App\Models\sanpham;
+use App\Models\User;
+use Carbon\Traits\Date;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Array_;
+use function Sodium\add;
 
 class AdminDashboardController extends Controller
 {
-    public function __construct()
+    private $user;
+    private $donhang;
+    private $danhgia;
+    private $chitietdonhang;
+    private $sanpham;
+    public function __construct(User $user,donhang $donhang,danhgia $danhgia,chitietdonhang $chitietdonhang,sanpham $sanpham)
     {
+        $this->user = $user;
+        $this->donhang = $donhang;
+        $this->danhgia = $danhgia;
+        $this->chitietdonhang = $chitietdonhang;
+        $this->sanpham = $sanpham;
     }
     public function index(){
-        return view('admins.dashboard.index');
+        $totalUer = $this->user->count();
+        $totalView = $this->sanpham->sum('view');
+        $totalDonHang = $this->donhang->count();
+        $totalDanhGia = $this->danhgia->count();
+
+
+        //doanh thu theo ngày
+        $donhang_created = $this->donhang->select(DB::raw('DATE(Created_at) as month'))
+            ->where('active',3)->distinct()->take(5)->orderBy('Created_at', 'asc')->pluck("month");
+        $ngay_doanhthu = array();
+        foreach ($donhang_created as $ngay){
+            $day = date('d', strtotime($ngay));
+            $month = date('m', strtotime($ngay));
+            $year = date('Y', strtotime($ngay));
+            $str = $day.'/'.$month.'/'.$year;
+            $donhang = $this->donhang
+                ->where('active',3)
+                ->whereDay('Created_at',$day)
+                ->whereMonth('Created_at',$month)
+                ->whereYear('Created_at',$year)->get();
+                $total = 0;
+                foreach ($donhang as $item){
+                    $total += $this->doanhthu($item->chitietdonhang);
+                }
+                $data_doanhthu[$str] = $total;
+        }
+        $ngay_doanhthu = array_keys($data_doanhthu);
+        //doanh thu theo đơn hàng
+        for($i=0;$i<4;$i++){
+            $donhang = $this->donhang->where('active',$i)->get();
+            $total = 0;
+            foreach ($donhang as $item){
+                $total += $this->doanhthu($item->chitietdonhang);
+            }
+            $doanhthu_donhang[$i] = $total;
+        }
+
+
+        return view('admins.dashboard.index',
+            compact('totalUer','totalView','totalDonHang','totalDanhGia',
+            'ngay_doanhthu','data_doanhthu','doanhthu_donhang'));
+    }
+    private function doanhthu($item){
+        $total = 0;
+        foreach ($item as $ctdn){
+            $total += doubleval($ctdn->soluong*doubleval($ctdn->dongia));
+        }
+        return $total;
     }
 }
--- resources/views/admins/dashboard/index.blade.php
+++ resources/views/admins/dashboard/index.blade.php
@@ -2,7 +2,9 @@
 @section('title')
     <title>Trang chủ Admin</title>
 @endsection
-
+@section('link_css')
+    <link rel="stylesheet" href="{{asset('admin_resources/dashboard/dashboard.css')}}">
+@endsection
 @section('container-fluid')
 
     <div class="row">
@@ -15,9 +17,178 @@
                     </ol>
                 </div>
                 <h2 class="page-title font-24">Dashboard</h2>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card widget-inline">
+                            <div class="card-body p-0">
+                                <div class="row g-0">
+                                    <div class="col-sm-6 col-xl-3">
+                                        <div class="card shadow-none m-0">
+                                            <div class="card-body text-center">
+                                                <i class="dripicons-user-group text-muted" style="font-size: 24px;"></i>
+                                                <h3><span>{{$totalUer}}</span></h3>
+                                                <p class="text-muted font-15 mb-0">User đăng ký</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6 col-xl-3">
+                                        <div class="card shadow-none m-0 border-start">
+                                            <div class="card-body text-center">
+                                                <i class="dripicons-preview text-muted" style="font-size: 24px;"></i>
+                                                <h3><span>{{$totalView}}</span></h3>
+                                                <p class="text-muted font-15 mb-0">Tổng lượt xem</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6 col-xl-3">
+                                        <div class="card shadow-none m-0 border-start">
+                                            <div class="card-body text-center">
+                                                <i class="dripicons-to-do text-muted" style="font-size: 24px;"></i>
+                                                <h3><span>{{$totalDonHang}}</span></h3>
+                                                <p class="text-muted font-15 mb-0">Tổng đơn hàng</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6 col-xl-3">
+                                        <div class="card shadow-none m-0 border-start">
+                                            <div class="card-body text-center">
+                                                <i class="dripicons-message text-muted" style="font-size: 24px;"></i>
+                                                <h3><span>{{$totalDanhGia}}</span></h3>
+                                                <p class="text-muted font-15 mb-0">Lượt đánh giá</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row">
+                        <div class="col-xl-6 order-lg-2 order-xl-1">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3 class="p-0 text-center">Doanh thu theo ngày</h3>
+                                    <div id="tongdoanhthu" class="font-18" style="min-height: 400px">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 order-lg-2 order-xl-1">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3 class="p-0 text-center">Doanh thu theo loại đơn hàng</h3>
+                                    <div id="doanhthudonhang" class="font-18" style="min-height: 400px">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
 @endsection
+@section('link_js')
+    <script src="{{asset('admin_resources/dashboard/apexcharts.js')}}"></script>
+    <script src="{{asset('admin_resources/dashboard/dashboard.js')}}"></script>
+    <script>
+        let ngay_doanhthu = <?php echo json_encode($ngay_doanhthu);?>;
+        let doanhthu_ngay = Object.values(<?php echo json_encode($data_doanhthu);?>);
+        let doanhthu_donhang = Object.values(<?php echo json_encode($doanhthu_donhang);?>);
 
+        let doanhthudonhang = {
+            series: doanhthu_donhang,
+            labels: ['Đơn hủy', 'Chờ duyệt', 'Đang giao', 'Đã giao'],
+            yaxis: {
+                show:false,
+                labels: {
+                    style: {
+                        fontSize: '16px'
+                    },
+                    formatter: function (value) {
+                        return value.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});
+                    }
+                },
+            },
+            chart: {
+                type: 'polarArea',
+            },
+            stroke: {
+                colors: ['#fff']
+            },
+            fill: {
+                opacity: 0.8
+            },
+            responsive: [{
+                breakpoint: 480,
+                options: {
+                    chart: {
+                        width: 200
+                    },
+                    legend: {
+                        position: 'bottom'
+                    }
+                }
+            }]
+        };
+        let tongdoanhthu = {
+            series: [{
+                name: "Doanh thu",
+                data: doanhthu_ngay,
+            }],
+            yaxis: {
+                labels: {
+                    style: {
+                        fontSize: '13px'
+                    },
+                    formatter: function (value) {
+                        return value.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});
+                    }
+                },
+            },
+
+            chart: {
+                type: 'line',
+                zoom: {
+                    enabled: false
+                }
+            },
+            dataLabels: {
+                enabled: true,
+            },
+            stroke: {
+                curve: 'straight'
+            },
+            title: {
+                text: `Doanh thu trong vòng ${ngay_doanhthu.length} ngày`,
+                align: 'left',
+            },
+            grid: {
+                row: {
+                    colors: ['#f3f3f3', 'transparent'],
+                    opacity: 0.5
+                },
+            },
+            xaxis: {
+                categories: ngay_doanhthu,
+                labels: {
+                    style: {
+                        fontSize: '14px'
+                    },
+                },
+            }
+        };
+
+        let doanhthu = new ApexCharts(document.querySelector("#tongdoanhthu"), tongdoanhthu);
+        doanhthu.render();
+        let donhang = new ApexCharts(document.querySelector("#doanhthudonhang"), doanhthudonhang);
+        donhang.render();
+
+    </script>
+@endsection
