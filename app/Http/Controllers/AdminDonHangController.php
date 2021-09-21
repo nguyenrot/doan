ad3386473dd0e6c88418d98367150b33b3200253
nguyenrot <69588951+nguyenrot@users.noreply.github.com>
2021-09-21 15:51:12 +0700

    Final doan

--- app/Http/Controllers/AdminDonHangController.php
+++ app/Http/Controllers/AdminDonHangController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\chitietdonhang;
 use App\Models\donhang;
+use App\Models\sanpham;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 
 class AdminDonHangController extends Controller
 {
     private $donhang;
-    public function __construct(donhang $donhang)
+    private $sanpham;
+    private $chitietdonhang;
+    public function __construct(donhang $donhang,sanpham $sanpham,chitietdonhang $chitietdonhang)
     {
         $this->donhang = $donhang;
+        $this->sanpham = $sanpham;
+        $this->chitietdonhang = $chitietdonhang;
     }
 
     public function choduyet(){
@@ -46,6 +52,13 @@ class AdminDonHangController extends Controller
     public function huydon(Request $request){
         Paginator::useBootstrap();
         $donhang = $this->donhang->find($request->id);
+        $ctdh = $this->chitietdonhang->where("donhang_id",$request->id)->get();
+        foreach ($ctdh as $item){
+            $sanpham = $this->sanpham->find($item->sanpham_id);
+            $this->sanpham->find($item->sanpham_id)->update([
+                'soluong'=> $sanpham->soluong + $item->soluong,
+            ]);
+        }
         $donhang->update([
            'active'=>0,
         ]);
